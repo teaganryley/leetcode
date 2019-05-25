b0063365b799464534854eb0c0f09542ad8eef66
teaganryley <teaganschultz@gmail.com>
2019-05-25 00:09:00 -0400

    first commit

---
+++ tests/test_solution1.py
@@ -0,0 +1,9 @@
+import os
+import sys
+
+# ensures pytest will locate project without having to run setup.py
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import pytest
+
+
