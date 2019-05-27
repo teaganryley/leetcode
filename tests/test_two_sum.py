be6b7af4ba96d118ea78344e7a47859e577ee125
teaganryley <teaganschultz@gmail.com>
2019-05-26 20:46:02 -0400

    parametrized pass test cases

--- tests/test_two_sum.py
+++ tests/test_two_sum.py
@@ -7,5 +7,27 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 import pytest
 from src.two_sum import TwoSum
 
-def test_solution_1():
-    """"""
+test_data_pass = [
+    ([2, 7, 11, 15], 9, [0, 1]),
+    ([3, 3], 6, [0, 1])
+]
+
+
+@pytest.fixture
+def sample_two_sum():
+    sample = TwoSum()
+    return sample
+
+
+@pytest.mark.parametrize('nums,target,expected', test_data_pass)
+def test_solution_1_pass(sample_two_sum, nums, target, expected):
+    """Test correctness of solution1"""
+    print('Array: {0} Target: {1} Expected: {2}'.format(nums, target, expected))
+    assert sample_two_sum.solution1(nums, target) == expected
+
+
+@pytest.mark.parametrize('nums,target,expected', test_data_pass)
+def test_solution_2_pass(sample_two_sum, nums, target, expected):
+    """Test correctness of solution2"""
+    print('Array: {0} Target: {1} Expected: {2}'.format(nums, target, expected))
+    assert sample_two_sum.solution2(nums, target) == expected
